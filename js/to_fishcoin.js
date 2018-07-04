9a2153cc8330cfd3b9525dc7889d1f2169e67985
jayshri93 <jay93totewad@gmail.com>
2018-07-04 11:10:39 +0530

    fishcoin changes

---
+++ js/to_fishcoin.js
@@ -0,0 +1,13 @@
+var toFishCoin = function(domain){
+
+	if(domain == "cell.fish"){ //cell.fish
+		console.log("in cellfish");
+		$('.fishcoin').remove();
+		// $('.cellfish').show();
+
+	}else if(domain === "fishcoin.club"){ //fishcoin domain
+		console.log("in fishcoin");
+		$('.cellfish').remove();
+		// $('.fishcoin').show();
+	}
+}
