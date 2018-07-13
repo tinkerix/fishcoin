d7bf7c1a574f9a75aa2c78c63ae33d753d29b584
jayshri93 <jay93totewad@gmail.com>
2018-07-13 17:30:37 +0530

    test with document.referrer

--- js/to_fishcoin.js
+++ js/to_fishcoin.js
@@ -1,11 +1,11 @@
-var toFishCoin = function(domain){
+var toFishCoin = function(ref){
 
-	if(domain == "cell.fish"){ //cell.fish
+	if(ref === ""){ //cell.fish
 		console.log("in cellfish");
 		$('.fishcoin').remove();
 		// $('.cellfish').show();
 
-	}else if(domain === "fishcoin.club"){ //fishcoin domain
+	}else if(ref === "http://fishcoin.club/"){ //fishcoin domain
 		console.log("in fishcoin");
 		$('.cellfish').remove();
 		// $('.fishcoin').show();
